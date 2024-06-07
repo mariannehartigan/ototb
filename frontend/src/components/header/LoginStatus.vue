6525b182be6d698ad37b61ffaaa0d5a1523d6f39
mariannehartigan <mariannehartigan@yahoo.ca>
2024-06-07 12:02:14 -0400

    right before designing for android studio

--- frontend/src/components/header/LoginStatus.vue
+++ frontend/src/components/header/LoginStatus.vue
@@ -1,15 +1,35 @@
 <template>
     <div class="loginStatus">
-        Login Status
+        <p>
+            Welcome, {{userName}} &nbsp;
+        
+      <button @click="handleAction">Logout</button>
+      </p>
     </div>
   </template>
   
-  <script>  
-  export default {
-    
-  }
+  <script setup lang="ts">  
+  import { storeToRefs } from 'pinia';
+  import { useUserStore } from 'src/stores/user-store';
+  import { computed } from 'vue';
+  import { useRouter } from 'vue-router';
+
+  const router = useRouter();
+  const { user } = storeToRefs(useUserStore());
+  const userName = computed(() => user.value?.name);
+  
+  const handleAction = async () => {
+    if (userName.value !== undefined) {
+      await useUserStore().logout();
+      await router.push('/guest/login');
+    } else {
+      await router.push('/guest/login');
+    }
+  };
+
   </script>
   
   <style>
   
   </style>
+  
